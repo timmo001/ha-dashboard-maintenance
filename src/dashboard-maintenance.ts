756e22c499c3e2ba1b52611cab49373ea6a746fe
Aidan Timson <aidan@timmo.dev>
2026-03-24 21:36:46 +0000

    Avaliability

--- src/dashboard-maintenance.ts
+++ src/dashboard-maintenance.ts
@@ -1,4 +1,5 @@
 import "./dashboard-maintenance/editor";
+import "./dashboard-maintenance/maintenance-availability-view-strategy";
 import "./dashboard-maintenance/maintenance-batteries-view-strategy";
 import "./dashboard-maintenance/maintenance-dashboard-strategy";
 import "./dashboard-maintenance/maintenance-summary-view-strategy";
